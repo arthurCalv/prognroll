538cf490a70f83f3d2541df00fd18c6939e9aee5
Arthur Graham <arthur@calvium.com>
2020-07-24 13:40:41 +0100

    modified version;

--- src/prognroll.js
+++ src/prognroll.js
@@ -1,18 +1,17 @@
-/* PrognRoll | https://mburakerman.github.io/prognroll/ | @mburakerman | License: MIT */
-(function ($) {
-    $.fn.prognroll = function (options) {
+/* PrognRoll (modified for Calvium) | https://mburakerman.github.io/prognroll/ | @mburakerman | License: MIT */
+(function($) {
+    $.fn.prognroll = function(options) {
 
         var settings = $.extend({
             height: 5, // progress bar height
             color: "#50bcb6", // progress bar background color
-            custom: false // if you make it true, you can add your custom div and see it's scroll progress on the page
         }, options);
 
         var progressBar = $("<span>", {
             class: "prognroll-bar",
         });
 
-        return this.each(function (i, el) {
+        return this.each(function(i, el) {
             if ($(this).data("prognroll")) {
                 return false;
             }
@@ -36,36 +35,22 @@
                 "bodyHeight": $(document).height()
             }
 
+            var calviumCustomDims = {
+                    "contentHeight": $('.content-wrapper').height()
+                }
+                // Custom dimension calculations for content
             function bindWindowScroll() {
-                $(window).scroll(function (e) {
+                $(window).scroll(function(e) {
                     e.preventDefault();
                     globals.windowScrollTop = $(window).scrollTop();
                     globals.windowOuterHeight = $(window).outerHeight();
                     globals.bodyHeight = $(document).height();
-
-                    var total = (globals.windowScrollTop / (globals.bodyHeight - globals.windowOuterHeight)) * 100;
+                    var total = (globals.windowScrollTop / (calviumCustomDims.contentHeight)) * 100;
                     $(".prognroll-bar").css("width", total + "%");
                 });
             }
 
-            if (settings.custom === false) {
-                bindWindowScroll();
-            } else {
-                // if el has no max-height set
-                if ($(this).css("max-height") == "none") {
-                    bindWindowScroll();
-                } else {
-                    $(this).scroll(function (e) {
-                        e.preventDefault();
-                        var customScrollTop = $(this).scrollTop();
-                        var customOuterHeight = $(this).outerHeight();
-                        var customScrollHeight = $(this).prop("scrollHeight");
-
-                        var total = (customScrollTop / (customScrollHeight - customOuterHeight)) * 100;
-                        $(".prognroll-bar").css("width", total + "%");
-                    });
-                }
-            }
+            bindWindowScroll();
 
             // get scroll position on on page load 
             var total = (globals.windowScrollTop / (globals.bodyHeight - globals.windowOuterHeight)) * 100;
